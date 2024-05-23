2478e980d63d9f385c992c9e2c91628c4f9ab071
Rainicer <Noramer@protonmail.com>
2024-05-23 13:39:26 +0800

    ruby-girl

---
+++ lib/ruby-girl.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ruby-girl/version"
+
+module RubyGirl
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/version.rb
@@ -0,0 +1,4 @@
+module MyTea
+  VERSION = '0.1.0'
+end
+
